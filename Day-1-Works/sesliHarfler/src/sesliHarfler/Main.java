415213024e157440d464fe7e9774a7bc1d4a92f9
Oğuzhan Arslan <72730335+ouzard@users.noreply.github.com>
2022-09-22 00:57:28 +0300

    JavaBootCamp2022

---
+++ Day-1-Works/sesliHarfler/src/sesliHarfler/Main.java
@@ -0,0 +1,21 @@
+package sesliHarfler;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		 char harf = 'j';
+	        
+	        switch(harf) {
+	               case 'A':
+	               case 'I':	   
+	               case 'O':
+	               case 'U':	 
+	            	   System.out.println("kaın sesli harf");
+	            	   break;
+	               default:
+	                   System.out.println("ince sesli harf");
+	        }
+	}
+
+}
